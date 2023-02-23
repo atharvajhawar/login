92cb776d1a3d947bc7c24d794d6e2b74978a7103
atharva jhawar <79523326+atharvajhawar@users.noreply.github.com>
2023-02-23 11:20:05 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+
+function Resetdata() {
+$("#empId").val("")
+$("#empName").val("");
+$("#empEmail").val("");
+$("#empId").focus();
+}
+
+function updatedata() {
+$("#empId").val("")
+$("#empName").val("");
+$("#empEmail").val("");
+$("#empId").focus();
+}
+
+function savedata()
+{
+$("#empId").val("")
+$("#empName").val("");
+$("#empEmail").val("");
+$("#empId").focus();
+}
+
+
